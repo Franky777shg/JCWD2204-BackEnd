f223f54d5bcf1edac4021570c354e23ffbf85909
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-10-18 15:50:17 +0700

    delete and edit done

--- simple-restAPI/index.js
+++ simple-restAPI/index.js
@@ -38,6 +38,35 @@ server.post("/userById/:id", (req, res) => {
 });
 
 // delete user by id
+server.delete("/delete/:id", (req, res) => {
+  database.filter((item) => item.id !== +req.params.id);
+  console.log(database);
+  //   console.log(id);
+  //   database.splice(id, 1);
+  res.status(200).send(database);
+});
+
 // edit user by id
+server.patch("/edit/:id", (req, res) => {
+  let user = database.filter((item) => item.id === +req.params.id)[0];
+  console.log(user);
+  console.log(req.body);
+  //   let propBody = Object.keys(req.body);
+  //   let propData = Object.keys(user);
+
+  //   propBody.map((item) => {
+  //     if (propData.indexOf(item) !== -1) {
+  //       user[item] = req.body[item];
+  //     }
+  //   });
+  for (let keys in user) {
+    if (req.body[keys]) {
+      user[keys] = req.body[keys];
+    }
+  }
+  console.log(user);
+  console.log(database);
+  res.status(200).send(database);
+});
 
 server.listen(PORT, () => console.log(`Server Running at PORT: ${PORT}`));
