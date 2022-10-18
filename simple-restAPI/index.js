e85ad130be7cec9c8c0cba26f5d142f70c23b1fd
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-10-18 14:01:23 +0700

    intro to express

--- simple-restAPI/index.js
+++ simple-restAPI/index.js
@@ -6,10 +6,12 @@ server.use(express.json());
 
 let database = [
   {
+    id: 0,
     name: "Budi",
     age: 12,
   },
   {
+    id: 1,
     name: "Anna",
     age: 14,
   },
@@ -19,9 +21,15 @@ server.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome to my API</h1>");
 });
 
+// register new user
 server.post("/register", (req, res) => {
-  console.log(req.body);
-  res.status(200).send("test");
+  database.push(req.body);
+  res.status(200).send(database);
 });
+
+// get all user
+// get user by id
+// delete user by id
+// edit user by id
 
 server.listen(PORT, () => console.log(`Server Running at PORT: ${PORT}`));
